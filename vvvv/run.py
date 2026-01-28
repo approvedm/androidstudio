11f66fb847e1e6eddd04e21754d717a77eb873e4
approvedm <126514054+approvedm@users.noreply.github.com>
2026-01-28 21:19:20 +0600

    Delete vvvv directory

--- vvvv/run.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-try:
-    import usdt
-    print("Checking function availability...")
-    asyncio.run(usdt.track_and_send())
-except AttributeError:
-    print("Error: track_and_send function not found in module.")
-except Exception as e:
-    print(f"Error: {e}")
